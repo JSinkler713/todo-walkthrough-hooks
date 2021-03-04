70e5b7cb4bfe19baa8ccc1125c8794e41c5070dc
James Sinkler <jamessinkler713@gmail.com>
2021-03-04 15:59:11 -0800

    change to functional components, no logic

--- solution-code/src/components/Header.js
+++ solution-code/src/components/Header.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react';
+import React from 'react'
 import {Link} from 'react-router-dom';
 
-class Header extends Component{
-  render(){
-    return (
-      <header>
-        <h1>ToDo</h1>
-        <nav>
-          <Link to={'/'}>Home</Link>  
-          <Link to={'/todos'}>Todos</Link>
-        </nav>
-      </header>
-    )
-  }
+function Header(){
+  return (
+    <header>
+      <h1>ToDo</h1>
+      <nav>
+        <Link to={'/'}>Home</Link>  
+        <Link to={'/todos'}>Todos</Link>
+      </nav>
+    </header>
+  )
 }
 
 export default Header
--- solution-code/src/components/Todos.js
+++ solution-code/src/components/Todos.js
@@ -3,8 +3,6 @@ import Todo from './Todo'
 
 class Todos extends Component {
 
-
-
   render(){
     let todos = this.props.todos.map( (todo) => {
       return (
