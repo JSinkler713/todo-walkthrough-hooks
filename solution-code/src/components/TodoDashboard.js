9142343269159a29a2694e4a9bd1897a3af4f989
James Sinkler <jamessinkler713@gmail.com>
2021-03-04 16:37:17 -0800

    all functional with hooks

--- solution-code/src/components/TodoDashboard.js
+++ solution-code/src/components/TodoDashboard.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react'
 
-class TodoDashboard extends Component {
-  render(){
-      return (
-        <div className="count-wrapper">
-          <p>You have <span className="count">{this.props.todoCount} todos</span> left to complete.</p>
-        </div>
-      )
-    }
+const TodoDashboard = (props) => {
+    return (
+      <div className="count-wrapper">
+        <p>You have <span className="count">{props.todoCount} todos</span> left to complete.</p>
+      </div>
+    )
 }
 
 export default TodoDashboard
