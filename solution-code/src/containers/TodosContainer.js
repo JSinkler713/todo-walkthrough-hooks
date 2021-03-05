c10fb1cf2ab44ab073baff427644fd47426f212a
James Sinkler <jamessinkler713@gmail.com>
2021-03-04 16:21:53 -0800

    refactor to hooks, and fix "left to complete" state updates

--- solution-code/src/containers/TodosContainer.js
+++ solution-code/src/containers/TodosContainer.js
@@ -1,140 +1,81 @@
-import React, {Component} from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoModel from '../models/Todo'
 import Todos from '../components/Todos'
 import TodoDashboard from '../components/TodoDashboard'
 import CreateTodoForm from '../components/CreateTodoForm'
 
 
-class TodosContainer extends Component {
-  constructor(){
-    super()
-    this.state = {
-      todos: [],
-      editingTodoId: null,
-      editing: false,
-      todoCount: 0
-    }
-  }
+function TodosContainer() {
+  const [todos, setTodos] = useState([])
+  const [editingTodoId, setEditingTodo] = useState(null)
+  const [editing, setEditing] = useState(false)
+  const [todoCount, setTodoCount] = useState(0)
   
-  componentDidMount(){
-    this.fetchData()
-  }
+  useEffect(()=> {
+    fetchData()
+  }, [])
 
-  fetchData = async() => {
-    
+  const fetchData = async() => {
     const res = await TodoModel.all()
-    this.setState({ 
-      todos: res.data.todos,
-      todo: '',
-      todoCount: res.data.todos.filter(todo=> todo.completed === false).length
-    })
-    /*
-    TodoModel.all().then( (res) => {
-      this.setState ({
-        todos: res.data.todos,
-        todo: '',
-        todoCount:res.data.todos.filter(todo => todo.completed === false).length
-      })
-    })
-    */
+    setTodos(res.data.todos)
+    setTodos(res.data.todos)
+    setTodoCount( res.data.todos.filter(todo=> todo.completed === false).length)
   }
 
-  createTodo = async(todo) => {
+  const createTodo = async(todo) => {
     let newTodo = {
         body: todo,
         completed: false
     }
     console.log('in create function')
     const anotherTodo = await TodoModel.create(newTodo)
-    let todos = this.state.todos
     let newTodos = [...todos, anotherTodo.data]
-    this.setState({ todos: newTodos })
-    /*
-    TodoModel.create(newTodo).then((res) => {
-        let todos = this.state.todos
-        let newTodos = todos.push(res.data)
-        this.setState({newTodos})
-
-    })
-    */
+    setTodos(newTodos)
+    setTodoCount( todoCount + 1)
   }
 
-  updateTodo = async (todoBody, todoId) => {
+  const updateTodo = async (todoBody, todoId) => {
     function isUpdatedTodo(todo) {
         return todo._id === todoId;
     }
     const result = await TodoModel.update(todoId, todoBody)
     let todos = this.state.todos
     todos.find(isUpdatedTodo).body = todoBody.body
-    this.setState({ todos: todos })
-    /*
-    TodoModel.update(todoId, todoBody).then((res) => {
-        let todos = this.state.todos
-        todos.find(isUpdatedTodo).body = todoBody.body
-        this.setState({todos: todos})
-    })
-    */
+    setTodos(todos)
   }
 
 
-  deleteTodo = async (todo) => {
+  const deleteTodo = async (todo) => {
+    if (!todo.completed) {
+      setTodoCount( todoCount - 1)
+    }
     let res = await TodoModel.delete(todo)
-    let todos = this.state.todos.filter(function(todo) {
+    let tempTodos = todos.filter(function(todo) {
       return todo._id !== res.data._id
     });
-    this.setState({todos})
-    /*
-    TodoModel.delete(todo).then((res) => {
-        let todos = this.state.todos.filter(function(todo) {
-          return todo._id !== res.data._id
-        });
-        this.setState({todos})
-    })
-    */
+    setTodos(tempTodos)
   }
 
-  markComplete = async(todoId, complete) => {
+  const markComplete = async(todoId, complete) => {
     console.log(complete)
     function isUpdatedTodo(todo) {
       return todo._id === todoId;
     } 
-    let todos = this.state.todos;
-    todos.find(isUpdatedTodo).complete = complete
-    this.setState({
-      todos: todos
-    })
+    let currentTodos = [...todos];
+    currentTodos.find(isUpdatedTodo).complete = complete
+    setTodos( currentTodos )
     const res = await TodoModel.update( todoId, complete )
     if (complete.completed) {
-      this.setState({
-        todoCount: this.state.todoCount - 1
-      })
+      setTodoCount( todoCount - 1)
     }
     else {
-      this.setState({
-        todoCount: this.state.todoCount + 1
-      })
+      setTodoCount( todoCount + 1 )
     }
     return res.data.completed
-
-    /*
-    TodoModel.update( todoId, complete ).then((res) => {
-      if(complete.completed){
-        this.setState({
-          todoCount: this.state.todoCount - 1
-        })
-      } else {
-        this.setState({
-          todoCount: this.state.todoCount + 1
-        })
-      }
-      return res.data.completed
-    })
-    */
   }
 
-  clearCompleted = () => {
+  const clearCompleted = () => {
     console.log("CLEAR!")
-    
     let filteredTodos = this.state.todos
       .filter( todo => {
         console.log("in filter")
@@ -143,32 +84,26 @@ class TodosContainer extends Component {
           TodoModel.delete(todo)
         }
           return true
-        
       })
-      this.setState({
-        todos: filteredTodos
-      })
-
+      setTodos(filteredTodos)
   }
   
-  render(){
-    return (
-      <div className='todosComponent'>
-        <CreateTodoForm
-          createTodo={ this.createTodo }
-          />
-        <Todos
-          todos={this.state.todos}
-          onDeleteTodo={this.deleteTodo} 
-          onUpdateTodo={this.updateTodo} 
-          markComplete={this.markComplete}
-          />
-        <TodoDashboard 
-          todoCount={this.state.todoCount} />
-
-      </div>
-    )
-  }
+  return (
+    <div className='todosComponent'>
+      <CreateTodoForm
+        createTodo={ createTodo }
+        />
+      <Todos
+        todos={todos}
+        onDeleteTodo={deleteTodo} 
+        onUpdateTodo={updateTodo} 
+        markComplete={markComplete}
+        />
+      <TodoDashboard 
+        todoCount={todoCount} />
+    </div>
+  )
 }
 
 export default TodosContainer
+
